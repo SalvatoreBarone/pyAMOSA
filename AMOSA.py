3d634d842d53e3064b819b37061f7ba46bb68bf5
Salvatore Barone <salvator.barone@gmail.com>
2022-05-07 19:59:48 +0200

    feat: allows the heuristic to run again, on previously provided solutions

--- AMOSA.py
+++ AMOSA.py
@@ -132,15 +132,34 @@ class AMOSA:
         self.__old_f = []
         self.__phy = []
 
+    def random_archive(self, problem):
+        print("Initializing random archive...")
+        num_of_initial_candidate_solutions = self.__archive_gamma * self.__archive_soft_limit
+        initial_candidate_solutions = [AMOSA.lower_point(problem), AMOSA.upper_point(problem)]
+        if self.__hill_climbing_iterations > 0:
+            for i in range(num_of_initial_candidate_solutions):
+                print(f"  {i + 1}/{num_of_initial_candidate_solutions}                                                  ", end = "\r", flush = True)
+                initial_candidate_solutions.append(AMOSA.hill_climbing(problem, AMOSA.random_point(problem), self.__hill_climbing_iterations))
+        for x in initial_candidate_solutions:
+            self.__add_to_archive(x)
+
+    def seeded_archive(self, problem, seeds_file):
+        print(f"Reading initial archive from {seeds_file}...")
+        file = open(seeds_file, "r")
+        file.readline() # ships the header
+        for row in file:
+            c = list(filter(None, row.replace("\n", "").split(";")))
+            assert len(c) == problem.num_of_variables, "Wrong amount of variables"
+            self.__add_to_archive(AMOSA.c_point(problem, [int(v) if t == AMOSA.Type.INTEGER else float(v) for v, t in zip(c, problem.types) ]))
+
     def minimize(self, problem):
         self.__parameters_check()
-        self.__archive = []
         self.__old_f = None
         self.__phy = []
         self.__ideal = None
         self.__nadir = None
         self.duration = time.time()
-        self.__initialize_archive(problem)
+        assert len(self.__archive) > 0, "Archive not initialized"
         if len(self.__archive) > self.__archive_hard_limit:
             self.__archive_clustering(problem)
         self.__print_header(problem)
@@ -150,30 +169,30 @@ class AMOSA:
         self.__print_statistics(problem)
         while self.__current_temperature > self.__final_temperature:
             for _ in range(self.__annealing_iterations):
-                y = random_perturbation(problem, x, self.__annealing_strength)
+                y = AMOSA.random_perturbation(problem, x, self.__annealing_strength)
                 fitness_range = self.__compute_fitness_range(x, y)
-                s_dominating_y = [s for s in self.__archive if dominates(s, y)]
+                s_dominating_y = [s for s in self.__archive if AMOSA.dominates(s, y)]
                 k_s_dominating_y = len(s_dominating_y)
-                s_dominated_by_y = [s for s in self.__archive if dominates(y, s)]
+                s_dominated_by_y = [s for s in self.__archive if AMOSA.dominates(y, s)]
                 k_s_dominated_by_y = len(s_dominated_by_y)
-                if dominates(x, y) and k_s_dominating_y >= 0:
-                    delta_avg = (sum([domination_amount(s, y, fitness_range) for s in s_dominating_y]) + domination_amount(x, y, fitness_range)) / (k_s_dominating_y + 1)
-                    if accept(sigmoid(-delta_avg * self.__current_temperature)):
+                if AMOSA.dominates(x, y) and k_s_dominating_y >= 0:
+                    delta_avg = (sum([AMOSA.domination_amount(s, y, fitness_range) for s in s_dominating_y]) + AMOSA.domination_amount(x, y, fitness_range)) / (k_s_dominating_y + 1)
+                    if AMOSA.accept(AMOSA.sigmoid(-delta_avg * self.__current_temperature)):
                         x = y
-                elif not dominates(x, y) and not dominates(y, x):
+                elif not AMOSA.dominates(x, y) and not AMOSA.dominates(y, x):
                     if k_s_dominating_y >= 1:
-                        delta_avg = sum([domination_amount(s, y, fitness_range) for s in s_dominating_y]) / k_s_dominating_y
-                        if accept(sigmoid(-delta_avg * self.__current_temperature)):
+                        delta_avg = sum([AMOSA.domination_amount(s, y, fitness_range) for s in s_dominating_y]) / k_s_dominating_y
+                        if AMOSA.accept(AMOSA.sigmoid(-delta_avg * self.__current_temperature)):
                             x = y
                     elif (k_s_dominating_y == 0 and k_s_dominated_by_y == 0) or k_s_dominated_by_y >= 1:
                         self.__add_to_archive(y)
                         if len(self.__archive) > self.__archive_soft_limit:
                             self.__archive_clustering(problem)
                         x = y
-                elif dominates(y, x):
+                elif AMOSA.dominates(y, x):
                     if k_s_dominating_y >= 1:
-                        delta_dom = [domination_amount(s, y, fitness_range) for s in s_dominating_y]
-                        if accept(sigmoid(min(delta_dom))):
+                        delta_dom = [AMOSA.domination_amount(s, y, fitness_range) for s in s_dominating_y]
+                        if AMOSA.accept(AMOSA.sigmoid(min(delta_dom))):
                             x = self.__archive[np.argmin(delta_dom)]
                     elif (k_s_dominating_y == 0 and k_s_dominated_by_y == 0) or k_s_dominated_by_y >= 1:
                         self.__add_to_archive(y)
@@ -181,7 +200,7 @@ class AMOSA:
                             self.__archive_clustering(problem)
                         x = y
                 else:
-                    raise RuntimeError(f"Something went wrong\narchive: {self.__archive}\nx:{x}\ny: {y}\n x < y: {dominates(x, y)}\n y < x: {dominates(y, x)}\ny domination rank: {k_s_dominated_by_y}\narchive domination rank: {k_s_dominating_y}")
+                    raise RuntimeError(f"Something went wrong\narchive: {self.__archive}\nx:{x}\ny: {y}\n x < y: {AMOSA.dominates(x, y)}\n y < x: {AMOSA.dominates(y, x)}\ny domination rank: {k_s_dominated_by_y}\narchive domination rank: {k_s_dominating_y}")
             self.__n_eval += self.__annealing_iterations
             self.__print_statistics(problem)
             self.__check_early_termination()
@@ -268,23 +287,12 @@ class AMOSA:
         if self.__cooling_factor <= 0 or self.__cooling_factor >= 1:
             raise RuntimeError("The cooling factor for the temperature of the matter must be in the (0, 1) range")
 
-    def __initialize_archive(self, problem):
-        print("Initializing archive...")
-        num_of_initial_candidate_solutions = self.__archive_gamma * self.__archive_soft_limit
-        initial_candidate_solutions = [lower_point(problem), upper_point(problem)]
-        if self.__hill_climbing_iterations > 0:
-            for i in range(num_of_initial_candidate_solutions):
-                print(f"  {i + 1}/{num_of_initial_candidate_solutions}                                                  ", end = "\r", flush = True)
-                initial_candidate_solutions.append(hill_climbing(problem, random_point(problem), self.__hill_climbing_iterations))
-        for x in initial_candidate_solutions:
-            self.__add_to_archive(x)
-
     def __add_to_archive(self, x):
         if len(self.__archive) == 0:
             self.__archive.append(x)
         else:
-            self.__archive = [y for y in self.__archive if not dominates(x, y)]
-            if not any([dominates(y, x) or is_the_same(x, y) for y in self.__archive]):
+            self.__archive = [y for y in self.__archive if not AMOSA.dominates(x, y)]
+            if not any([AMOSA.dominates(y, x) or AMOSA.is_the_same(x, y) for y in self.__archive]):
                 self.__archive.append(x)
 
     def __archive_clustering(self, problem):
@@ -292,13 +300,13 @@ class AMOSA:
             feasible = [s for s in self.__archive if all([g <= 0 for g in s["g"]])]
             non_feasible = [s for s in self.__archive if all([g > 0 for g in s["g"]])]
             if len(feasible) > self.__archive_hard_limit:
-                do_clustering(feasible, self.__archive_hard_limit)
+                AMOSA.do_clustering(feasible, self.__archive_hard_limit)
                 self.__archive = feasible
             else:
-                do_clustering(non_feasible, self.__archive_hard_limit - len(feasible))
+                AMOSA.do_clustering(non_feasible, self.__archive_hard_limit - len(feasible))
                 self.__archive = non_feasible + feasible
         else:
-            do_clustering(self.__archive, self.__archive_hard_limit)
+            AMOSA.do_clustering(self.__archive, self.__archive_hard_limit)
 
     def __remove_infeasible(self, problem):
         if problem.num_of_constraints > 0:
@@ -352,133 +360,143 @@ class AMOSA:
         f = [s["f"] for s in self.__archive] + [x["f"], y["f"]]
         return np.max(f, axis = 0) - np.min(f, axis = 0)
 
+    @staticmethod
+    def hill_climbing(problem, x, max_iterations):
+        d, up = AMOSA.hill_climbing_direction(problem)
+        for _ in range(max_iterations):
+            y = copy.deepcopy(x)
+            AMOSA.hill_climbing_adaptive_step(problem, y, d, up)
+            if AMOSA.dominates(y, x) and AMOSA.not_the_same(y, x):
+                x = y
+            else:
+                d, up = AMOSA.hill_climbing_direction(problem, d)
+        return x
 
-def hill_climbing(problem, x, max_iterations):
-    d, up = hill_climbing_direction(problem)
-    for _ in range(max_iterations):
-        y = copy.deepcopy(x)
-        hill_climbing_adaptive_step(problem, y, d, up)
-        if dominates(y, x) and not_the_same(y, x):
-            x = y
+    @staticmethod
+    def lower_point(problem):
+        x = {
+            "x": problem.lower_bound,
+            "f": [0] * problem.num_of_objectives,
+            "g": [0] * problem.num_of_constraints if problem.num_of_constraints > 0 else None}
+        AMOSA.get_objectives(problem, x)
+        return x
+
+    @staticmethod
+    def upper_point(problem):
+        x = {
+            "x": problem.upper_bound,
+            "f": [0] * problem.num_of_objectives,
+            "g": [0] * problem.num_of_constraints if problem.num_of_constraints > 0 else None}
+        AMOSA.get_objectives(problem, x)
+        return x
+
+    @staticmethod
+    def random_point(problem):
+        x = {
+            "x": [l if l == u else random.randrange(l, u) if t == AMOSA.Type.INTEGER else random.uniform(l, u) for l, u, t in zip(problem.lower_bound, problem.upper_bound, problem.types)],
+            "f": [0] * problem.num_of_objectives,
+            "g": [0] * problem.num_of_constraints if problem.num_of_constraints > 0 else None}
+        AMOSA.get_objectives(problem, x)
+        return x
+
+    @staticmethod
+    def c_point(problem, c):
+        assert len(c) == problem.num_of_variables, "Wrong number of variables"
+        x = {
+            "x": c,
+            "f": [0] * problem.num_of_objectives,
+            "g": [0] * problem.num_of_constraints if problem.num_of_constraints > 0 else None}
+        AMOSA.get_objectives(problem, x)
+        return x
+
+    @staticmethod
+    def random_perturbation(problem, s, strength):
+        z = copy.deepcopy(s)
+        indexes = random.sample(range(problem.num_of_variables), random.randrange(1, 1 + min([strength, problem.num_of_variables])))
+        for i in indexes:
+            l = problem.lower_bound[i]
+            u = problem.upper_bound[i]
+            t = problem.types[i]
+            z["x"][i] = l if l == u else random.randrange(l, u) if t == AMOSA.Type.INTEGER else random.uniform(l, u)
+        AMOSA.get_objectives(problem, z)
+        return z
+
+    @staticmethod
+    def hill_climbing_direction(problem, c_d = None):
+        if c_d is None:
+            return random.randrange(0, problem.num_of_variables), 1 if random.random() > 0.5 else -1
         else:
-            d, up = hill_climbing_direction(problem, d)
-    return x
-
-
-def random_point(problem):
-    x = {
-        "x": [l if l == u else random.randrange(l, u) if t == AMOSA.Type.INTEGER else random.uniform(l, u) for l, u, t in zip(problem.lower_bound, problem.upper_bound, problem.types)],
-        "f": [0] * problem.num_of_objectives,
-        "g": [0] * problem.num_of_constraints if problem.num_of_constraints > 0 else None}
-    get_objectives(problem, x)
-    return x
-
-
-def lower_point(problem):
-    x = {
-        "x": problem.lower_bound,
-        "f": [0] * problem.num_of_objectives,
-        "g": [0] * problem.num_of_constraints if problem.num_of_constraints > 0 else None}
-    get_objectives(problem, x)
-    return x
-
-
-def upper_point(problem):
-    x = {
-        "x": problem.upper_bound,
-        "f": [0] * problem.num_of_objectives,
-        "g": [0] * problem.num_of_constraints if problem.num_of_constraints > 0 else None}
-    get_objectives(problem, x)
-    return x
-
-
-def random_perturbation(problem, s, strength):
-    z = copy.deepcopy(s)
-    indexes = random.sample(range(problem.num_of_variables), random.randrange(1, 1 + min([strength, problem.num_of_variables])))
-    for i in indexes:
-        l = problem.lower_bound[i]
-        u = problem.upper_bound[i]
-        t = problem.types[i]
-        z["x"][i] = l if l == u else random.randrange(l, u) if t == AMOSA.Type.INTEGER else random.uniform(l, u)
-    get_objectives(problem, z)
-    return z
-
-
-def hill_climbing_direction(problem, c_d = None):
-    if c_d is None:
-        return random.randrange(0, problem.num_of_variables), 1 if random.random() > 0.5 else -1
-    else:
-        up = 1 if random.random() > 0.5 else -1
-        d = random.randrange(0, problem.num_of_variables)
-        while c_d == d:
+            up = 1 if random.random() > 0.5 else -1
             d = random.randrange(0, problem.num_of_variables)
-        return d, up
+            while c_d == d:
+                d = random.randrange(0, problem.num_of_variables)
+            return d, up
 
-
-def hill_climbing_adaptive_step(problem, s, d, up):
-    lower_bound = problem.lower_bound[d] - s["x"][d]
-    upper_bound = problem.upper_bound[d] - s["x"][d]
-    if (up == -1 and lower_bound == 0) or (up == 1 and upper_bound == 0):
-        return 0
-    if problem.types[d] == AMOSA.Type.INTEGER:
-        step = random.randrange(lower_bound, 0) if up == -1 else random.randrange(0, upper_bound + 1)
-        while step == 0:
+    @staticmethod
+    def hill_climbing_adaptive_step(problem, s, d, up):
+        lower_bound = problem.lower_bound[d] - s["x"][d]
+        upper_bound = problem.upper_bound[d] - s["x"][d]
+        if (up == -1 and lower_bound == 0) or (up == 1 and upper_bound == 0):
+            return 0
+        if problem.types[d] == AMOSA.Type.INTEGER:
             step = random.randrange(lower_bound, 0) if up == -1 else random.randrange(0, upper_bound + 1)
-    else:
-        step = random.uniform(lower_bound, 0) if up == -1 else random.uniform(0, upper_bound)
-        while step == 0:
+            while step == 0:
+                step = random.randrange(lower_bound, 0) if up == -1 else random.randrange(0, upper_bound + 1)
+        else:
             step = random.uniform(lower_bound, 0) if up == -1 else random.uniform(0, upper_bound)
-    s["x"][d] += step
-    get_objectives(problem, s)
+            while step == 0:
+                step = random.uniform(lower_bound, 0) if up == -1 else random.uniform(0, upper_bound)
+        s["x"][d] += step
+        AMOSA.get_objectives(problem, s)
 
+    @staticmethod
+    def do_clustering(archive, hard_limit):
+        while len(archive) > hard_limit:
+            d = np.array([[np.linalg.norm(np.array(i["f"]) - np.array(j["f"])) if not np.array_equal(np.array(i["x"]), np.array(j["x"])) else np.nan for j in archive] for i in archive])
+            try:
+                i_min = np.nanargmin(d)
+                r = int(i_min / len(archive))
+                c = i_min % len(archive)
+                del archive[r if np.where(d[r] == np.nanmin(d[r]))[0].size > np.where(d[c] == np.nanmin(d[c]))[0].size else c]
+            except:
+                # print("Clustering cannot be performed anymore")
+                return
 
-def do_clustering(archive, hard_limit):
-    while len(archive) > hard_limit:
-        d = np.array([[np.linalg.norm(np.array(i["f"]) - np.array(j["f"])) if not np.array_equal(np.array(i["x"]), np.array(j["x"])) else np.nan for j in archive] for i in archive])
-        try:
-            i_min = np.nanargmin(d)
-            r = int(i_min / len(archive))
-            c = i_min % len(archive)
-            del archive[r if np.where(d[r] == np.nanmin(d[r]))[0].size > np.where(d[c] == np.nanmin(d[c]))[0].size else c]
-        except:
-            # print("Clustering cannot be performed anymore")
-            return
+    @staticmethod
+    def get_objectives(problem, s):
+        out = {"f": [0] * problem.num_of_objectives,
+               "g": [0] * problem.num_of_constraints if problem.num_of_constraints > 0 else None}
+        problem.evaluate(s["x"], out)
+        s["f"] = out["f"]
+        s["g"] = out["g"]
 
+    @staticmethod
+    def is_the_same(x, y):
+        return x["x"] == y["x"]
 
-def get_objectives(problem, s):
-    out = {"f": [0] * problem.num_of_objectives,
-           "g": [0] * problem.num_of_constraints if problem.num_of_constraints > 0 else None}
-    problem.evaluate(s["x"], out)
-    s["f"] = out["f"]
-    s["g"] = out["g"]
+    @staticmethod
+    def not_the_same(x, y):
+        return x["x"] != y["x"]
 
+    @staticmethod
+    def dominates(x, y):
+        if x["g"] is None:
+            return all(i <= j for i, j in zip(x["f"], y["f"])) and any(i < j for i, j in zip(x["f"], y["f"]))
+        else:
+            return ((all(i <= 0 for i in x["f"]) and any(i > 0 for i in y["g"])) or  # x is feasible while y is not
+                    (any(i > 0 for i in x["g"]) and any(i > 0 for i in y["g"]) and all([i <= j for i, j in zip(x["g"], y["g"])]) and any([i < j for i, j in zip(
+                        x["g"], y["g"])])) or  # x and y are both infeasible, but x has a lower constraint violation
+                    (all(i <= 0 for i in x["g"]) and all(i <= 0 for i in y["g"]) and all([i <= j for i, j in zip(x["f"], y["f"])]) and any([i < j for i, j in zip(
+                        x["f"], y["f"])])))  # both are feasible, but x dominates y in the usual sense
 
-def is_the_same(x, y):
-    return x["x"] == y["x"]
+    @staticmethod
+    def accept(probability):
+        return random.random() < probability
 
+    @staticmethod
+    def domination_amount(x, y, r):
+        return np.prod([abs(i - j) / k for i, j, k in zip(x["f"], y["f"], r)])
 
-def not_the_same(x, y):
-    return x["x"] != y["x"]
-
-
-def dominates(x, y):
-    if x["g"] is None:
-        return all(i <= j for i, j in zip(x["f"], y["f"])) and any(i < j for i, j in zip(x["f"], y["f"]))
-    else:
-        return ((all(i <= 0 for i in x["f"]) and any(i > 0 for i in y["g"])) or  # x is feasible while y is not
-                (any(i > 0 for i in x["g"]) and any(i > 0 for i in y["g"]) and all([i <= j for i, j in zip(x["g"], y["g"])]) and any([i < j for i, j in zip(
-                    x["g"], y["g"])])) or  # x and y are both infeasible, but x has a lower constraint violation
-                (all(i <= 0 for i in x["g"]) and all(i <= 0 for i in y["g"]) and all([i <= j for i, j in zip(x["f"], y["f"])]) and any([i < j for i, j in zip(
-                    x["f"], y["f"])])))  # both are feasible, but x dominates y in the usual sense
-
-
-def accept(probability):
-    return random.random() < probability
-
-
-def domination_amount(x, y, r):
-    return np.prod([abs(i - j) / k for i, j, k in zip(x["f"], y["f"], r)])
-
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(np.array(-x, dtype = np.float128)))
+    @staticmethod
+    def sigmoid(x):
+        return 1 / (1 + np.exp(np.array(-x, dtype = np.float128)))
