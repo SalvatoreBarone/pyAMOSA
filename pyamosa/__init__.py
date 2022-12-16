4051234f9eab18337717b8ffb402de2794b77b5c
Salvatore <salvator.barone@gmail.com>
2022-12-16 16:41:25 +0100

    fix: fix the early termination criterion bug

--- pyamosa/__init__.py
+++ pyamosa/__init__.py
@@ -1,5 +1,5 @@
 from .pyamosa import *
 name = "pyamosa"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __author__ = "Salvatore Barone"
 __credits__ = "Department of Electrical Engineering and Information Technologies, University of Naples Federico II, Via Claudio 21, Naples, Italy"
