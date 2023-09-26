579011ceadbb1107e12b92b58e657d5b1a4789a0
Salvatore Barone <salvatore.barone@unina.it>
2023-09-26 08:34:01 +0200

    feat: ls-mop test problems

--- problems/DTLZ2.py
+++ problems/DTLZ2.py
@@ -16,7 +16,6 @@ Street, Fifth Floor, Boston, MA 02110-1301, USA.
 """
 import pyamosa, numpy as np, itertools
 
-
 """
 For further details, please refer to
 
@@ -34,7 +33,8 @@ For further details, please refer to
     https://doi.org/10.3929/ETHZ-A-004287264.
 
 """
-class DTZ1(pyamosa.Problem):
+
+class DTZ2(pyamosa.Problem):
 
     def __init__(self, n_vars, n_objs):
         self.k = n_vars - n_objs + 1
@@ -42,11 +42,13 @@ class DTZ1(pyamosa.Problem):
 
     def evaluate(self, x, out):
         f = np.zeros((self.num_of_objectives,))
-        g = 100 * ( self.k + np.sum( np.square(x[self.num_of_objectives-1:] - 0.5) - np.cos(20 * np.pi * (x[self.num_of_objectives-1:] - 0.5)) ) )
-        f[0] = 0.5 * (1 + g) * np.prod(x[:self.num_of_objectives-2])
+        g = np.sum( np.square(x[self.num_of_objectives-1:] - 0.5) )
+        f[0] = 0.5 * (1 + g) * np.prod( np.cos(np.pi * 0.5* x[:self.num_of_objectives-2]))
+
         for m in range(1, self.num_of_objectives-1):
-            f[m] = 0.5 * (1 + g) * (1 - x[self.num_of_objectives-m]) * np.prod(x[:self.num_of_objectives-m-1])
-        f[self.num_of_objectives-1] = 0.5 * (1 + g) * (1 - x[0])
+            f[m] = (1 + g) * np.sin(np.pi * 0.5 * x[self.num_of_objectives-m]) * np.prod( np.cos(np.pi * 0.5 * x[:self.num_of_objectives-m-1]))
+
+        f[self.num_of_objectives-1] = (1 + g) * np.sin(np.pi * 0.5 * x[0])
         out["f"] = f.tolist()
 
     def optimums(self):
@@ -55,7 +57,7 @@ class DTZ1(pyamosa.Problem):
         for element in itertools.product(*candidate_pareto_set):
             c = {"x": element + [0] * self.self.k, "f": [0] * self.num_of_objectives, "g": [0] * self.num_of_constraints if self.num_of_constraints > 0 else None}
             self.evaluate(c["x"], c)
-            if np.sum(c["f"][:self.num_of_objectives-1]) - 0.5 < np.finfo(float).eps:
+            if np.sum(c["f"][:self.num_of_objectives-1] ** 2) - 1 < np.finfo(float).eps:
                 pareto_front.append(c)
         return pareto_front
 
